5d0d45fe6c32118710771e8b6bf1efecb9034389
qasim <qasimzaildar2@gmail.com>
2023-01-04 11:55:39 -0800

    first commit

---
+++ TestApp/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using TaxCalculatorInterviewTests;
+
+namespace TestProject
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            TaxCalculator customer1 = new TaxCalculator();
+
+            Console.WriteLine($"Customer 1 Default Tax for Transport: {customer1.GetCurrentTaxRate(Commodity.Transport)}");
+
+            customer1.SetCustomTaxRate(Commodity.Transport, 12.5);
+            customer1.SetCustomTaxRate(Commodity.Transport, 13.5);
+            customer1.SetCustomTaxRate(Commodity.Transport, 14.5);
+
+            Console.WriteLine($"Customer 1 Transport Tax: {customer1.GetCurrentTaxRate(Commodity.Transport)}");
+
+
+            TaxCalculator customer2 = new TaxCalculator();
+
+            Console.WriteLine($"Customer 2 Default Tax For Transport: {customer2.GetCurrentTaxRate(Commodity.Transport)}");
+
+            customer2.SetCustomTaxRate(Commodity.Transport, 1.5);
+            customer2.SetCustomTaxRate(Commodity.Transport, 3.5);
+            customer2.SetCustomTaxRate(Commodity.Transport, 20.5);
+
+            Console.WriteLine($"Customer 2 Transport Tax: {customer2.GetCurrentTaxRate(Commodity.Transport)}");
+
+
+
+        }
+    }
+}
